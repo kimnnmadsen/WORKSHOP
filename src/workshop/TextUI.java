4f060483f07a8f562427601d5e2b9259da9eabc4
Kim Madsen <kmads18@student.sdu.dk>
2018-11-19 14:11:50 +0100

    init TextUI

---
+++ src/workshop/TextUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop;
+
+/**
+ *
+ * @author Kim Madsen <kmads18@student.sdu.dk>
+ */
+public class TextUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
